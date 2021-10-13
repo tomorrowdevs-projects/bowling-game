9b5afefc17cbad8fd5ca2d2105d071ecea7fbbd7
Diana <bernabei.d@gmail.com>
2021-10-13 23:40:51 +0200

    upd: fix languages

--- js/index.js
+++ js/index.js
@@ -8,5 +8,5 @@ for (let i = 1; i <= 10 + play.extraRoll; i++) {
     play.roll(pins);
 }
 
-// 3h per studiare jest e testare il funzionamento sulla classe base  -> ok
-// 2h per implementare le funzioni con i test
+// 3h study jest and testing basics function  -> ok
+// 1.5h for implement all js functions
