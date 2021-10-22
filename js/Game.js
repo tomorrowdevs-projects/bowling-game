2b4d89b880187fa5c13dc7e024f9080fc3307f1b
Diana <bernabei.d@gmail.com>
2021-10-22 17:19:23 +0200

    fix: game functions

--- js/Game.js
+++ js/Game.js
@@ -5,23 +5,30 @@ module.exports = class Game {
         this.score = 0;         // total score
         this.frame = 0;         // max 10 frames
         this.extraRoll = 0;     // max +3 extra rolls
+        this.precPins = 0;
     }
 
     roll(pins){
-        let precPins = this.pins;
+        this.precPins = this.pins;
         this.pins = pins;
         this.score += this.pins;
-        
-        if(this.pins == 10){
-            console.log("strike (actual pins ",pins," - prec pins ",precPins," )")
-            this.extraRoll += ((this.extraRoll <= 3) ? 2 : 0);
-        }
-        if(this.pins + precPins == 10){
-            console.log("spares (actual pins ",pins," - prec pins ",precPins," )")
-            this.extraRoll += ((this.extraRoll <= 3) ? 1 : 0);
-        }
 
-        console.log("frame ",this.frame," - pins = ",this.pins, "score = ",this.score);
+        console.log("frame ",this.frame," pins = ",this.pins," precpins = ",this.precPins, "score = ",this.score, " extraroll = ",this.extraRoll);
+
+        if(this.extraRoll <= 1 && this.extraRoll >= 0){
+            //strike
+            if(this.pins == 10 ) {
+                this.extraRoll += 2;
+                console.log("strike (actual pins ",pins," - prec pins ",this.precPins," ) extraroll = ",this.extraRoll)
+                return
+            }
+            //spare
+            if(this.pins + this.precPins == 10){
+                this.extraRoll += 1;
+                console.log("spares (actual pins ",pins," - prec pins ",this.precPins," ) extraroll = ",this.extraRoll)
+            }      
+        }
+        
     }
 
     getScore(){
