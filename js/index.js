f21d1f2b72d9161047985296814e37dfe582a144
Diana <bernabei.d@gmail.com>
2021-10-22 17:18:09 +0200

    upd: time tracking

--- js/index.js
+++ js/index.js
@@ -8,5 +8,6 @@ for (let i = 1; i <= 10 + play.extraRoll; i++) {
     play.roll(pins);
 }
 
-// 3h study jest and testing basics function  -> ok
-// 1.5h for implement all js functions
+// 3h study jest and testing basics function  
+// 1h for implement all js functions 
+// 2h for test the functions and fix bugs 
